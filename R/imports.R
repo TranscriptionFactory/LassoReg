51f9b09e5cd17db26bf723de85f5cdd3bef7abae
aar126 <transcriptionfactory@gmail.com>
2023-02-16 16:00:47 -0500

    imports

--- R/imports.R
+++ R/imports.R
@@ -1,28 +1,28 @@
 
-
-required_pkgs = c("tidyverse",
-                  "ggrepel", "gridExtra", "devtools",
-                  "ggpubr", "stats", "ggpmisc",
-                  "glmnet", "e1071", "caret", "randomForest",
-                  "tree", "gbm", "matrixStats", "cvAUC",
-                  "pROC", "pls",
-                  "ROCR", "BAS", "gmp", "matrixcalc",
-                   "EssReg")
-
-for (p in required_pkgs) {
-  installed = require(p, character.only = TRUE)
-  if (p == "Essreg") {
-    devtools::install_github("Hanxi-002/EssReg", force = FALSE,
-                             dependencies = TRUE)
-  }
-  # else if (p == "LassoReg") {
-  #   devtools::install_github("TranscriptionFactory/LassoReg", force = FALSE,
-  #                            dependencies = TRUE)
-  # }
-  else if (installed == FALSE) {
-      install.packages(p, dependencies = TRUE)
-  }
-
-  library(p, character.only = TRUE)
-
-}
+#
+# required_pkgs = c("tidyverse",
+#                   "ggrepel", "gridExtra", "devtools",
+#                   "ggpubr", "stats", "ggpmisc",
+#                   "glmnet", "e1071", "caret", "randomForest",
+#                   "tree", "gbm", "matrixStats", "cvAUC",
+#                   "pROC", "pls",
+#                   "ROCR", "BAS", "gmp", "matrixcalc",
+#                    "EssReg")
+#
+# for (p in required_pkgs) {
+#   installed = require(p, character.only = TRUE)
+#   if (p == "Essreg") {
+#     devtools::install_github("Hanxi-002/EssReg", force = FALSE,
+#                              dependencies = TRUE)
+#   }
+#   # else if (p == "LassoReg") {
+#   #   devtools::install_github("TranscriptionFactory/LassoReg", force = FALSE,
+#   #                            dependencies = TRUE)
+#   # }
+#   else if (installed == FALSE) {
+#       install.packages(p, dependencies = TRUE)
+#   }
+#
+#   library(p, character.only = TRUE)
+#
+# }
