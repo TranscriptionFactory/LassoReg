1a1d0bbefd341af017f116fcb728c9649bc2568f
Transcription Factory <transcriptionfactory@gmail.com>
2023-04-03 18:48:06 -0400

    Update getModules.R
    typo

--- R/getModules.R
+++ R/getModules.R
@@ -22,7 +22,7 @@ processFile = function(cluster_input, df, fullModules = F, filepath = NULL) {
   } else if (!is.null(cluster_input)) {
       # read through clusters dataframe
       for (i in 1:length(cluster_input)) {
-        allModules[length(allModules) + 1] = list( stringr::str_c(cluster_input[1, ]))
+        allModules[length(allModules) + 1] = list( stringr::str_c(cluster_input[i, ]))
       }
   } else {
       # somethings wrong
